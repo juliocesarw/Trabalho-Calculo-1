0a71b59107cc7ce30e840b8f387443acab9c9dcb
lucasszanotelli <lucasszanotelli@gmail.com>
2024-11-06 15:51:03 -0300

    kbvkj

--- trabalho_calc1.py
+++ trabalho_calc1.py
@@ -1,11 +1,12 @@
+##CALCULAR A QUANTIDADE DE PLANTAS DE UM CULTIVO NUMA DETERMINADA ÁREA  
+#SABER QUAL A MEDIDA DE ESPAÇAMENTO DESEJADO PARA CULTIVO E A ÁREA TOTAL DO TERRENO
+
 def menu():
     print("O que você quer saber?")
     print("[1] - Valor das raízes")
     print("[2] - Valor máximo")
     print("[3] - Valor das mínimo")
-    print("[4] - Valor das raízes")
-    print("[5] - Valor das raízes")
-
+    print("[4] - Mostrar Gráfico")
 
 
 #PRINCIPAL
