59cc54ca6f531241f4a5d686ef0eb2dbaee37ccc
Júlio César <juliocesarw099@gmail.com>
2024-11-06 16:23:46 -0300

    números aleatórios

--- trabalho_calc1.py
+++ trabalho_calc1.py
@@ -1,3 +1,14 @@
+from random import randint
+
+def gerar_numero_aleatorio():
+    var = randint(1,10)
+    return var
+
+def gerar_angulo():
+    vetor = [30, 45, 60]
+    angulo = randint(0,2)
+    return vetor[angulo]
+
 def menu():
     print("O que você quer saber?")
     print("[1] - Valor das raízes")
@@ -6,6 +17,9 @@ def menu():
     print("[4] - Valor das raízes")
     print("[5] - Valor das raízes")
 
+#principal
 
+var = gerar_angulo()
+print(var)
 
 #PRINCIPAL
