d01ee8e95592c142d55e4fc5302bccbd923e8d20
Júlio César <juliocesarw099@gmail.com>
2024-11-06 15:31:17 -0300

    Commit

---
+++ trabalho_calc1.py
@@ -0,0 +1,9 @@
+def menu():
+    print("O que você quer saber?")
+    print("[1] - Valor das raízes")
+    print("[2] - Valor máximo")
+    print("[3] - Valor das mínimo")
+    print("[4] - Valor das raízes")
+    print("[5] - Valor das raízes")
+
+#PRINCIPAL
