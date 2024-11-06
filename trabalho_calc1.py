c6a0b306e43a0de866ee585a6e7014b23d79fa26
lucasszanotelli <lucasszanotelli@gmail.com>
2024-11-06 16:26:52 -0300

    Master

--- trabalho_calc1.py
+++ trabalho_calc1.py
@@ -1,17 +1,13 @@
-#CALCULAR A ALTURA MÁXIMA DE UM PROJÉTIL SABENDO A VELOCIDADE INICIAL
+#CALCULAR A ALTURA MÁXIMA DE UM PROJÉTIL SABENDO A VELOCIDADE INICIAL (aleatório)  E O ÂNGULO ( 30,45,60 ) -> aleatorio
 #CALCULAR A DISTÂNCIA PERCORRIDA PELO PROJÉTIL
 #INSERIR UM JOGO PARA QUE O USUÁRIO INFORME UM POSSÍVEL VALOR DE DISTÂNCIA
 #SE ACERTAR, GANHA PONTO
 #PLOTAR O GRÁFICO
 
 def menu():
-    print("O que você quer saber?")
-    print("[1] - Valor das raízes")
-    print("[2] - Valor máximo")
-    print("[3] - Valor das mínimo")
-    print("[4] - Mostrar Gráfico")
+    print("MENU")
 
 print("julio")
 
 
-#PRINCIPAL
+#PRINCI, 
